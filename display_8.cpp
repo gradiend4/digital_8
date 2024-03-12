632a3627184050d94390d20ea93274fff2280d50
gradiend4 <162925621+gradiend4@users.noreply.github.com>
2024-03-12 17:57:09 +0300

    Add files via upload

--- display_8.cpp
+++ display_8.cpp
@@ -4,7 +4,7 @@ void Display_8::record(uint8_t digital) {             // (цифра)
   for (int i = 0; i < 2; i++) digitalWrite(i + pin_Disp, 1);  // гасим катоды, высокий уровень
   for (int i = 0; i < 8; i++) {
     digitalWrite(pin_Disp+4, 0); //clock pin CP 
-    digitalWrite(pin_Disp+3, (Digs[digital]) & 1 << i); // data pin A
+    digitalWrite(pin_Disp+3, Digs[digital] & 1 << i); // data pin A
     digitalWrite(pin_Disp+4, 1); //
   }
 }
@@ -12,5 +12,14 @@ void Display_8::init(){
 	for (int i = pin_Disp; i <= pin_Disp+4 ; i++) pinMode(i, OUTPUT);
 }
 void Display_8::print(uint8_t addr){
+	
 	for (int y = 0; y < 4; y++) digitalWrite(pin_Disp+3-y, addr & 1 <<y); // addr(0-7) знакоместо
 }	
+void Display_8::record_byte(uint8_t _byte) {             // (цифра)
+  for (int i = 0; i < 2; i++) digitalWrite(i + pin_Disp, 1);  // гасим катоды, высокий уровень
+  for (int i = 0; i < 8; i++) {
+    digitalWrite(pin_Disp+4, 0); //clock pin CP 
+    digitalWrite(pin_Disp+3, _byte & 1 << i); // data pin A
+    digitalWrite(pin_Disp+4, 1); //
+  }
+}
--- display_8.h
+++ display_8.h
@@ -1,15 +1,15 @@
 #pragma once
 #include <Arduino.h>
 
-const byte Digs[] = { B11111100, B01100000, B11011010, B11110010, B01100110, B10110110, B10111110, B11100000, B11111110, B11110110, B00000000 };  // от 0 до 9,10 пусто
+const byte Digs[] = { B11111100, B01100000, B11011010, B11110010, B01100110, B10110110, B10111110, B11100000, B11111110, B11110110, B11111101, B01100001, B11011011, B11110011, B01100111, B10110111, B10111111, B11100001, B11111111, B11110111 };  // от 0 до 9
 const byte pin_Disp= 2;
 
 class Display_8 {
 	public:
 	void record(uint8_t digital);
+	void record_byte(uint8_t _byte);
 	void print(uint8_t addr);
 	void init();
 	private:
-	
 };
 
